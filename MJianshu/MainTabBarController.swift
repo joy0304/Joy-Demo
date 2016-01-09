33bf0dcacffc8334669e116246c5d0257395b3d6
张星宇 <649395594@qq.com>
2016-01-10 00:30:23 +0800

    修改tabbar中间图标在pop和push时的bug

--- MJianshu/MainTabBarController.swift
+++ MJianshu/MainTabBarController.swift
@@ -27,14 +27,12 @@ extension MainTabBarController{
         addChildVC(FindViewController(), title: "发现", image: "icon_tabbar_home", selected: "icon_tabbar_home_active")
         // 关注
         addChildVC(FollowViewController(), title: "关注", image: "icon_tabbar_subscription", selected: "icon_tabbar_subscription_active")
-        //先设置一个空视图，确保tabbar被五等分
-        addChildViewController(UIViewController())
+        // 添加中间的按钮
+        addCenterButton(imageName: "button_write")
         // 简友
         addChildVC(FriendsViewController(), title: "简友圈", image: "icon_tabbar_activity", selected: "icon_tabbar_activity_active")
         // 我的
         addChildVC(MyViewController(), title: "我的", image: "icon_tabbar_me", selected: "icon_tabbar_me_active")
-        // 添加中间按钮
-        addCenterButton(btnImage: UIImage(named: "button_write")!, selectedImg: UIImage(named: "button_write")!, selector: "writeArticleButtonClicked", view: view)
     }
     
     /**
@@ -56,38 +54,40 @@ extension MainTabBarController{
         let childNaviagation = UINavigationController(rootViewController: childVC)
         addChildViewController(childNaviagation)
     }
-
 }
 
 // MARK: - 中间按钮
 extension MainTabBarController{
-    /**
-     //添加TabBar中间按钮
-     
-     :param: buttonImage 默认状态下图片
-     :param: selectedimg 选中状态下图片
-     :param: selector    点击按钮调用的方法
-     :param: view        这个按钮被加载的视图
-     */
-    func addCenterButton(btnImage buttonImage: UIImage, selectedImg selectedimg: UIImage, selector: String, view: UIView) {
-        button = UIButton(type: UIButtonType.Custom)
-        button.frame = CGRectMake(0, 0, tabbarBigImageLength, tabbarBigImageLength)
-        button.center = tabBar.center
-        view.addSubview(button)
-
-        button.setImage(buttonImage, forState: UIControlState.Normal)
-        button.setImage(selectedimg, forState: UIControlState.Selected)
-        
-        button.adjustsImageWhenDisabled = true  // 设置选中时阴影
-        button.addTarget(self, action: Selector(selector), forControlEvents:UIControlEvents.TouchUpInside)
-    }
-        
     /**
      点击按钮触发的函数，present写新文章的页面
      */
     func writeArticleButtonClicked() {
         let writeViewController = UIStoryboard(name: "Write", bundle: nil).instantiateViewControllerWithIdentifier("writeViewController")
         self.presentViewController(writeViewController, animated: true, completion: nil)
+    }
+    
+    /**
+     添加中间的图片按钮
+     
+     :param: imageName 图片名称
+     */
+    func addCenterButton(imageName imageName: String) {
+        let containerVC = UIViewController()
+        let buttonImage = UIImage(named: imageName)?.imageWithRenderingMode(.AlwaysOriginal)
+        
+        containerVC.tabBarItem.image = buttonImage
+        containerVC.tabBarItem.tag = 2
+        containerVC.tabBarItem.imageInsets = UIEdgeInsetsMake(5, 0, -5, 0)
+        
+        let childNaviagation = UINavigationController(rootViewController: containerVC)
+        addChildViewController(childNaviagation)
+    }
+    
+    override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        switch item.tag {
+        case 2: writeArticleButtonClicked()
+        default: break
+        }
     }
 }
 
